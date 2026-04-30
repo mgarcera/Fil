4e9b308fc91c50431171e8a5d11fa8c0778f48e4
Mason Garcera <mgarcera@asafehaven.org>
2026-04-30 17:14:53 -0500

    Initial Commit

---
+++ Fil/FilApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilApp.swift
+//  Fil
+//
+//  Created by Mason Garcera on 4/30/26.
+//
+
+import SwiftUI
+
+@main
+struct FilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
